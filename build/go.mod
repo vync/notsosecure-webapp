e8e03f9b8032a89a4fc5e715ad4437ed645e53aa
vzambianchi <vzambianchi@thoughtmachine.net>
2021-12-09 12:26:41 +0000

    changes the application code to have the reflected XSS happening on the first get request.

--- build/go.mod
+++ build/go.mod
@@ -1,3 +1,3 @@
 module notsosecure-webserver.example.com/notsosecure-webserver
 
-go 1.17
+go 1.16
--- build/main.go
+++ build/main.go
@@ -7,20 +7,29 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		name := r.FormValue("name")
-		address := r.FormValue("address")
+	//	if r.Method == "POST" {
+	//		if err := r.ParseForm(); err != nil {
+	//			fmt.Fprintf(w, "ParseForm() err: %v", err)
+	//			return
+	//		}
+	//		name := r.FormValue("name")
+	//		address := r.FormValue("address")
+	//		w.Header().Set("Content-Type", "text/html")
+	//		fmt.Fprintf(w, "Name = %s\n", name)
+	//		fmt.Fprintf(w, "Address = %s\n", address)
+	//		return
+	//	}
+	//
+	//	http.ServeFile(w, r, "form.html")
+	if r.Method == "GET" {
+		id := r.URL.Query()["id"]
 		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(w, "Name = %s\n", name)
-		fmt.Fprintf(w, "Address = %s\n", address)
+		fmt.Fprintf(w, "id = %s\n", id)
 		return
 	}
 
 	http.ServeFile(w, r, "form.html")
+
 }
 
 func main() {
